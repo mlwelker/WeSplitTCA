35f1d78904b074946de6d4853e74fe471fbdf98d
mlwelker <nutria.watts.0g@icloud.com>
2023-10-06 17:56:34 -0500

    Initial Commit

---
+++ WeSplitTCA/WeSplitTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitTCAApp.swift
+//  WeSplitTCA
+//
+//  Created by Michael Welker on 2023-10-06.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
