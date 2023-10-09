718c5ebbc018aafc69c278674a1abe0958d556c2
mlwelker <nutria.watts.0g@icloud.com>
2023-10-09 10:19:04 -0500

    withViewStore checkAmountChanged tipPercentages

--- WeSplitTCA/ContentView.swift
+++ WeSplitTCA/ContentView.swift
@@ -7,30 +7,42 @@ struct WeSplit { }
 extension WeSplit: Reducer {
     var body: some ReducerOf<WeSplit> {
         Reduce { state, action in
-            return .none
+            switch action {
+            case .checkAmountChanged(let newAmount):
+                state.checkAmount = newAmount
+                return .none
+            }
         }
     }
     
     enum Action: Equatable {
-        
+        case checkAmountChanged(Double)
     }
     
     struct State: Equatable {
         var checkAmount = 0.0
         var numberOfPeople = 2
         var tipPercentage = 20
+        
+        let tipPercentages = [10, 15, 20, 25, 0] // does this belong in State?
     }
 }
 
 struct ContentView: View {
     let store: StoreOf<WeSplit>
     
-    let tipPercentages = [10, 15, 20, 25, 0]
-    
     var body: some View {
-        Form {
-            Section {
-//                TextField("Check Amount", text: )
+        WithViewStore(store, observe: { $0 }) { viewStore in
+            Form {
+                Section {
+                    TextField(
+                        "Check Amount",
+                        value: viewStore.binding(
+                            get: \.checkAmount,
+                            send: { .checkAmountChanged($0) }),
+                        format: .currency(code: "USD")
+                    )
+                }
             }
         }
     }
